4eef23132d688caacf2d64e1bb20c83e8793adb7
Paulius Mickevicius <pmickevicius@outlook.com>
2021-09-25 09:53:38 +0000

    uploading works

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-from flask import Flask, request 
+from flask import Flask, request, render_template, session
+import os
+from flask.wrappers import Response
+
+uploads = "/home/paulius/face/uploads"
+allowed_extentions = {
+    "jpg", "png", "jpeg"
+}
 
 app = Flask(__name__)
+app.config["uploads"] = uploads
+
+def allowed_file(filename):
+    return "." in filename and \
+        filename.rsplit(".", 1)[1].lower() in allowed_extentions
+
+@app.route("/")
+def Homepage():
+    return {
+        "Route to machine learning upload": "/ml-upload",
+        "Route to recognition upload": "/rec-upload"
+    }
+
+@app.route("/ml-upload", methods=["POST"])
+def ml_upload():
+    files = request.files.getlist("file_name")
+    uploaded = []
+    for file in files:
+        if allowed_file(file.filename):
+            upload_file = file.filename
+            uploaded.append(upload_file)
+            file.save(os.path.join(app.config["uploads"], "machine-learning", upload_file))
+            print(uploaded)
+    return {"uploaded": uploaded}
 
 
-@app.route('/')
-def index():
-    return {'message': 'Welcome to the FACE!'}
+@app.route("/rec-upload", methods=["POST"])
+def rec_upload():
+    file = request.files["file_name"]
+    if allowed_file(file.filename):
+        file.save(os.path.join(app.config["uploads"], "recognition", file.filename))
+        return {"uploaded": file.filename}
+    else:
+        return {"message": "file format not supported"}
 
 
-@app.route('/paulius', methods=["GET"])
-def funkcija():
-    print(request.remote_addr)
-    ip_address = request.remote_addr
-    return {'Jusu IP adresas': ip_address}
-
+if __name__ == "__main__":
+    app.run(debug=True)
--- facial_recognition/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from .routes import index_recognition, function_recognition
