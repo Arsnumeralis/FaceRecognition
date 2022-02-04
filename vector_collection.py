7d03e2f0f62a3b13f8e74e2534ba09f84206dfc1
Arsnumeralis <76921379+Arsnumeralis@users.noreply.github.com>
2022-02-04 09:49:56 +0000

    removing redundancy

--- vector_collection.py
+++ vector_collection.py
@@ -7,18 +7,6 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("./data_files/shape_predictor_5_face_landmarks.dat")
 recognition_model = dlib.face_recognition_model_v1("./data_files/dlib_face_recognition_resnet_model_v1.dat")
 
-def vector_collection_local(name):
-    for filename in os.listdir(os.path.join("ref", name)):
-        reference_img_path = os.path.join("ref", name, filename)
-        reference_img = dlib.load_rgb_image(reference_img_path)
-        detected_reference = detector(reference_img, 1)
-        print(filename, len(detected_reference))
-        reference_shape = predictor(reference_img, detected_reference[0])
-        aligned_reference = dlib.get_face_chip(reference_img, reference_shape)
-        reference_rep = recognition_model.compute_face_descriptor(aligned_reference)
-        reference_rep = numpy.array(reference_rep)
-        numpy.save(os.path.join(f"ref/{name}", filename), reference_rep)
-
 def vector_collection(name, file, data):
     # reference_img = dlib.load_rgb_image(file) #for testing
     reference_img = data
